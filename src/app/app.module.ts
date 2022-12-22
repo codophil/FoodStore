cb4a45493a6e6edbb2f05123b57b0e27a27f2744
Mohammad Ali <moama71@gmail.com>
2022-12-22 14:38:19 +0330

    Adding Search Bar - Angular Part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StarRatingModule } from 'angular-star-rating';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, StarRatingModule.forRoot()],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StarRatingModule.forRoot(),
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
