0f7f7ea85ffb4fb7c54da2c120bed35ea644632e
Mohammad Ali <moama71@gmail.com>
2022-12-25 18:51:33 +0330

    Adding Tags Bar - Angular Part

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,12 +18,12 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       if (params['searchTerm']) {
-        this.foods = this.foodService
-          .getAll()
-          .filter((food) =>
-            food.name.toLowerCase().includes(params['searchTerm'].toLowerCase())
-          );
-      } else this.foods = this.foodService.getAll();
+        this.foods = this.foodService.getAllFoodsBySearchTerm(
+          params['searchTerm']
+        );
+      } else if (params['tag'])
+        this.foods = this.foodService.getAllFoodsByTag(params['tag']);
+      else this.foods = this.foodService.getAll();
     });
   }
 }
