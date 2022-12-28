954e39c0df0e2b7d65db2912753113b49f8a7748
Mohammad Ali <moama71@gmail.com>
2022-12-28 12:29:33 +0330

    Food Page - CSS

--- src/app/tags/tags.component.ts
+++ src/app/tags/tags.component.ts
@@ -8,14 +8,14 @@ import { Tag } from '../shared/models/Tag';
   styleUrls: ['./tags.component.css'],
 })
 export class TagsComponent implements OnInit {
-  
   @Input()
   foodPageTags?: string[];
+  @Input()
+  justifyContent: string = 'center';
   tags?: Tag[];
   constructor(private foodService: FoodService) {}
 
   ngOnInit(): void {
-    if(!this.foodPageTags)
-    this.tags = this.foodService.getAllTags();
+    if (!this.foodPageTags) this.tags = this.foodService.getAllTags();
   }
 }
